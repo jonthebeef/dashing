0ee60a8e35794228945508ecdf1e9629a0f3d407
jon grant <jon@alfieandjon.com>
2013-03-18 12:23:53 +0000

    dashboard stuff

---
+++ sweet_dashboard_project/jobs/sample_job.rb
@@ -0,0 +1,5 @@
+# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+SCHEDULER.every '1m', :first_in => 0 do |job|
+  # send_event('widget_id', { })
+  send_event('widget_id', json_formatted_data)
+end
