8eaac37486427f23a89b58b35f5554f558e6eaf2
jon grant <jon@alfieandjon.com>
2013-03-18 11:56:22 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dashing::Application.config.secret_token = 'abbc17588d85911c1aa9fc90584b833576fd89f4225ac58eaa0e6a04a3a0a9c04ee5995e5d9b832af20f7a235ad744e5f839a22396ced39b7771a91a92c7467d'
